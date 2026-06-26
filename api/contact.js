bbf0c0c8d89909e9199bba6c64dba2bacec46f6f
HARSH <harsh@HARSHs-MacBook-Air.local>
2026-06-26 17:15:19 +0530

    Fix SPA routing

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,8 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+console.log("API Key Exists:", !!process.env.RESEND_API_KEY);
+console.log("API Key Prefix:", process.env.RESEND_API_KEY?.substring(0, 5));
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -13,7 +15,7 @@ export default async function handler(req, res) {
     const { name, email, message } = req.body;
 
     await resend.emails.send({
-      from: "FINAIW <onboarding@resend.dev>",
+      from: "FINAIW <harshrockz63@gmail.com>",
       to: "fashanime009@gmail.com",
       subject: "📩 New Contact Form Submission",
 
@@ -34,11 +36,13 @@ export default async function handler(req, res) {
       success: true,
     });
 
-  } catch (error) {
-    console.error(error);
+} catch (error) {
+  console.error(JSON.stringify(error, null, 2));
 
-    return res.status(500).json({
-      success: false,
-    });
-  }
+  return res.status(500).json({
+    success: false,
+    error: error.message,
+    details: error,
+  });
+}
 }
